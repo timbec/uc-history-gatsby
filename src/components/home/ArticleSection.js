5bc455a4f6e41398c3608c1343a2d87a01b9f662
Tim Beckett <timbeckett@ymail.com>
2019-06-30 11:38:05 -0400

    Articles show up on front page.

--- src/components/home/ArticleSection.js
+++ src/components/home/ArticleSection.js
@@ -1,11 +1,50 @@
 import React from 'react'
+import { Link, graphql, useStaticQuery } from 'gatsby';
+
+import PostFragment from '../postItems/postFragment'; 
 
 const ArticleSection = () => {
+    const data = useStaticQuery(graphql`
+            query {
+                allWordpressWpWriting(limit: 2) {
+                edges {
+                    node {
+                        title
+                        date
+                        excerpt
+                        slug
+                        wordpress_id
+                        featured_media {
+                            source_url
+                          }
+                    }
+                }
+                }
+            }
+        `)
+    
     return (
         <div>
             <h1>Articles</h1>
+            <ul>
+                {
+                    data.allWordpressWpWriting.edges.map((post) => {
+                        return (
+                            <li key={post.node.wordpress_id} 
+                            className="post__link">
+                                                <figure className="post__image">
+                                                <img src={post.node.featured_media.source_url} />
+                                                </figure>
+                                            <h2 className="post__title" dangerouslySetInnerHTML={{ __html: post.node.title }} />
+                                            <p className="post__escerpt" dangerouslySetInnerHTML={{ __html: post.node.excerpt }} />
+                                        </li>
+                        )
+                    })
+                }
+            </ul>
         </div>
     )
 }
+
 
 export default ArticleSection
--- src/components/postitems/postFragment.js
+++ src/components/postitems/postFragment.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
-const postFragment = () => {
+const PostFragment = (post) => {
+    // const post = { title, slug, excerpt }
+    console.log(post);
     return (
-        <div>
-            
-        </div>
+            <li key={post.node.wordpress_id} 
+            className="post__link">
+                                <figure className="post__image">
+                                <img src={post.node.featured_media.source_url} />
+                                </figure>
+                            <h2 className="post__title" dangerouslySetInnerHTML={{ __html: post.node.title }} />
+                            <p className="post__escerpt" dangerouslySetInnerHTML={{ __html: post.node.excerpt }} />
+                        </li>
     )
 }
 
-export default postFragment
+export default PostFragment
